bf097f306f6ffb8542cca082bb04a36704da2334
ashearouse <andy.shearouse@gmail.com>
2015-04-15 15:27:36 -0500

    Added listeners for different ImageButtons

--- app/src/main/java/com/example/andrewshearouse11/nongameapp/MainActivity.java
+++ app/src/main/java/com/example/andrewshearouse11/nongameapp/MainActivity.java
@@ -1,17 +1,61 @@
 package com.example.andrewshearouse11.nongameapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageButton campusTourButton;
+    ImageButton campusMapButton;
+    ImageButton eventsCalendarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //establish ImageView objects
+        campusTourButton = (ImageButton) findViewById();
+        campusMapButton = (ImageButton) findViewById();
+        eventsCalendarButton = (ImageButton) findViewById();
+
+        //set the click listeners
+        setListeners();
+    }
+
+    private void setListeners(){
+        //launch campus tour
+        campusTourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent campusTour = new Intent(getBaseContext(), CampusTour.class);
+                startActivity(campusTour);
+            }
+        });
+
+        //launch campus map
+        campusMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent campusMap = new Intent(getBaseContext(), CampusMap.class);
+                startActivity(campusMap);
+            }
+        });
+
+        //launch full events calendar
+        eventsCalendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent eventsCalendar = new Intent(getBaseContext(), EventsCalendar.class);
+                startActivity(eventsCalendar);
+            }
+        });
     }
 
 
