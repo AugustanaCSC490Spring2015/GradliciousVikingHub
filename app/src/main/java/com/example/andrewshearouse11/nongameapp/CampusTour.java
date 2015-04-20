dae8027ee472a6ed7cb2edb74730aa0dac3c94a5
ashearouse <andrewshearouse11@augustana.edu>
2015-04-20 14:57:28 -0500

    Updated Java files

--- app/src/main/java/com/example/andrewshearouse11/nongameapp/CampusTour.java
+++ app/src/main/java/com/example/andrewshearouse11/nongameapp/CampusTour.java
@@ -1,7 +1,36 @@
 package com.example.andrewshearouse11.nongameapp;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
 /**
  * Created by Ethan on 4/16/2015.
  */
-public class CampusTour {
+public class CampusTour extends Activity implements OnMapReadyCallback {
+
+    GoogleMap mainMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.campus_map_layout);
+
+        //Setup Map Fragment
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapviewfragment);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mainMap = map;
+    }
+
 }
