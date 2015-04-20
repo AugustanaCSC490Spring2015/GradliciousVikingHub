5e3b165ed650f7abb1d043c0d54799fb73f5be61
ethantanner <ethantanner@me.com>
2015-04-19 23:03:02 -0500

    Fixed campusTour.java causing crash when activity changed. Campus tour now has google map with location change listener. Trial geofence around Hanson Hall, toasts message when inside geofence. Added XML parsing class. Began work on xml parsing code.

--- app/src/main/java/com/example/andrewshearouse11/nongameapp/AugustanaCampusTour.java
+++ app/src/main/java/com/example/andrewshearouse11/nongameapp/AugustanaCampusTour.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
@@ -13,7 +14,7 @@ import com.google.android.gms.maps.model.*;
 /**
  * Created by Ethan on 4/16/2015.
  */
-public class CampusMap extends Activity implements OnMapReadyCallback {
+public class AugustanaCampusTour extends Activity implements OnMapReadyCallback {
 
     GoogleMap mainMap;
 
@@ -46,8 +47,16 @@ public class CampusMap extends Activity implements OnMapReadyCallback {
         @Override
         public void onMyLocationChange (Location location) {
 
+            if(inBounds(location, new LatLng(41.503272, -90.551696), new LatLng(41.504288, -90.550880))){
+                Toast.makeText(getBaseContext(), "At Hanson", Toast.LENGTH_LONG) .show();
+            }
 
         }
     };
 
+    public Boolean inBounds(Location location, LatLng southWest, LatLng northEast) {
+        double locationLat = location.getLatitude();
+        double locationLng = location.getLongitude();
+        return (locationLat >= southWest.latitude && locationLat <= northEast.latitude && locationLng <= southWest.longitude && locationLng >= northEast.longitude);
+    }
 }
--- app/src/main/java/com/example/andrewshearouse11/nongameapp/CampusTour.java
+++
@@ -1,7 +0,0 @@
-package com.example.andrewshearouse11.nongameapp;
-
-/**
- * Created by Ethan on 4/16/2015.
- */
-public class CampusTour {
-}
