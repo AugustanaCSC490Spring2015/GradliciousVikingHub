52d5cc1b6806ef9ffaec81415b72c0b5efa69381
ethantanner <ethantanner@me.com>
2015-05-08 15:11:05 -0500

    Fixed bug causing crash when using gps.

--- app/src/main/java/com/example/andrewshearouse11/nongameapp/ListViewArrayListManager.java
+++ app/src/main/java/com/example/andrewshearouse11/nongameapp/ListViewArrayListManager.java
@@ -29,7 +29,6 @@ public class ListViewArrayListManager {
 
     public ListViewArrayListManager(Context context){
         this.context = context;
-        mainArrayList = new ArrayList<Building>();
         defaultBuildingsArrayList = new ArrayList<Building>();
         proximateBuildingsArrayList = new ArrayList<Building>();
         numProximateBuildings = 0;
@@ -61,69 +60,21 @@ public class ListViewArrayListManager {
                 }
             }
 
-            mainArrayList.add(new Building(entry.getKey(), new LatLng(buildingLat,buildingLng), buildingInformation));
+            defaultBuildingsArrayList.add(new Building(entry.getKey(), new LatLng(buildingLat,buildingLng), buildingInformation));
         }
-
-        for(Building building : mainArrayList){
-            defaultBuildingsArrayList.add(building);
-        }
-        Log.w("defaultBuildings", defaultBuildingsArrayList.toString());
-    }
-
-    private void pullFromParse(){
-
-        final ParseQuery<ParseObject> query = ParseQuery.getQuery("AcademicBuildings");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> parseObjects, ParseException e) {
-
-                if(e==null){
-                    int i = 0;
-                    while(i < parseObjects.size()){
-                        String buildingName = parseObjects.get(i).getString("buildingName");
-                        String buildingInfo = "YEAR OPENED: " + parseObjects.get(i).getInt("yearOpened");
-                        buildingInfo += "\nMAJORS: " + parseObjects.get(i).getString("majors");
-                        buildingInfo += "\nBUILDING SERVICES: " + parseObjects.get(i).getString("buildingServices");
-                        mainArrayList.add(new Building(buildingName ,new LatLng(0,0), buildingInfo));
-                        i++;
-                    }
-                }else{
-                    Log.d("info", "Sad Panda");
-                }
-                Log.w("nothing left in parseQuery","");
-            }
-        });
-    }
-
-    public void clearMainArrayList(){
-        mainArrayList.clear();
     }
 
     public void addProximateBuilding(Building building){
-        mainArrayList.add(numProximateBuildings, building);
-        numProximateBuildings ++;
         proximateBuildingsArrayList.add(building);
     }
     public void removeProximateBuilding(Building building){
-        for(int i = 0; i <= numProximateBuildings; i++){
-            if(mainArrayList.get(i).equals(building)){
-                mainArrayList.remove(i);
-                numProximateBuildings --;
-            }
-        }
         proximateBuildingsArrayList.remove(building);
     }
     public boolean contains(Building building){
-        return mainArrayList.contains(building);
+        return defaultBuildingsArrayList.contains(building) || proximateBuildingsArrayList.contains(building);
     }
     public boolean containsInProximate(Building building){
         return proximateBuildingsArrayList.contains(building);
-    }
-    public Building getBuilding(int position){
-        return mainArrayList.get(position);
-    }
-    public ArrayList<Building> getMainArrayList(){
-        return mainArrayList;
     }
     public ArrayList<Building> getDefaultBuildingsArrayList(){
         return defaultBuildingsArrayList;
