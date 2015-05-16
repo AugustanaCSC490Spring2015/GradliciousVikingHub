4ac6d3e2337d2d02efb47980af8a915270830fff
ashearouse <Swimmerboy08>
2015-05-16 12:55:40 -0500

    Events calendar changed to only pull date, title, and description of events AND IT'S WORKING!!!!!

--- app/src/main/java/edu/augustana/csc490/vikinghub/EventsCalendar.java
+++ app/src/main/java/edu/augustana/csc490/vikinghub/EventsCalendar.java
@@ -18,6 +18,7 @@ import java.util.Date;
 
 public class EventsCalendar extends Activity {
 
+        //URL of Augustana's HTML calendar
         private final String URL_STRING = "http://www.augustana.edu/prebuilt/acal/calpage.php?mode=js&viewid=13";
 
         @Override
@@ -28,16 +29,16 @@ public class EventsCalendar extends Activity {
 
             TextView display = (TextView)findViewById(R.id.info);
 
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(new Date());
-            String month = "&month=" + cal.get(Calendar.MONTH);
-            String year = "&year=" + cal.get(Calendar.YEAR);
+//            Calendar cal = Calendar.getInstance();
+//            cal.setTime(new Date());
+//            String month = "&month=" + cal.get(Calendar.MONTH);
+//            String year = "&year=" + cal.get(Calendar.YEAR);
 
-            String url = URL_STRING+year+month;
+            String url = URL_STRING;//+year+month;
             Log.w("URL",url);
             HTMLParser parser = new HTMLParser();
             parser.sendDisplay(display);
-            parser.execute(url);
+            parser.execute(URL_STRING);
 
         }
 
@@ -49,6 +50,7 @@ public class EventsCalendar extends Activity {
     }
 }
 
+//Code to run in background and parse HTML
 class HTMLParser extends AsyncTask<String, Void, String> {
     TextView display;
     @Override
@@ -60,7 +62,22 @@ class HTMLParser extends AsyncTask<String, Void, String> {
             Document doc  = Jsoup.connect(strings[0]).get();
             Log.d("JSwa", "Connected to ["+strings[0]+"]");
 
-            uglyDoc = doc.toString();
+            //uglyDoc = doc.toString();
+            Elements eventTitles = doc.getElementsByAttributeValueContaining("class", "cal_title");
+            Elements eventDates = doc.getElementsByAttributeValueContaining("class", "cal_date");
+            Elements eventDescriptions = doc.getElementsByAttributeValueContaining("class", "cal_desc");
+            for(int i = 0; i < eventTitles.size(); i++){
+                Log.d("JSwa", "date"+eventDates.get(i).text());
+                uglyDoc = uglyDoc + eventTitles.get(i).text() + "\n" + eventDates.get(i).text() + "\n" + eventDescriptions.get(i).text() + "\n";
+            }
+
+//            Elements eventTitles = doc.select("span.\\&quot;cal_title\\&quot;");
+//            Elements eventDates = doc.select("div.\\&quot;cal_date\\&quot;");
+//            Elements eventDescriptions = doc.select("div.\\&quot;cal_desc\\&quot;");
+//            for(int i = 0; i < eventTitles.size(); i++){
+//                Log.d("JSwa", "date"+eventDates.get(i).text());
+//                uglyDoc = uglyDoc + eventTitles.get(i).text() + "\n" + eventDates.get(i).text() + "\n" + eventDescriptions.get(i).text() + "\n";
+//            }
             String[] uglyDocArray = uglyDoc.split("\n");
             for(String line : uglyDocArray){
                 Log.w("line",line);
