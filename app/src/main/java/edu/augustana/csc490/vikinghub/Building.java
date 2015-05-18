b2bd709fb93e0721f892fc9921033639a883e223
ashearouse <Swimmerboy08>
2015-05-18 10:30:18 -0500

    Removed DownloadXMLTask.java class (no longer needed), fixed typo, and added comments

--- app/src/main/java/edu/augustana/csc490/vikinghub/Building.java
+++ app/src/main/java/edu/augustana/csc490/vikinghub/Building.java
@@ -5,6 +5,7 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by ethanwojcinski11 on 5/1/2015.
  */
+//Class to define Building objects
 public class Building implements Comparable{
 
     String buildingName;
@@ -24,7 +25,7 @@ public class Building implements Comparable{
         this.buildingInfo = buildingInfo;
         this.buildingRadius = buildingRadius;
     }
-
+    //getter methods
     public int getBuildingRadius(){
         return buildingRadius;
     }
@@ -40,9 +41,12 @@ public class Building implements Comparable{
     public String getBuildingInfo(){
         return buildingInfo;
     }
+
     public Boolean equals(Building testBuilding){
         return buildingName.equals(testBuilding.getBuildingName());
     }
+
+    //used to sort buildings
     public int compareTo(Object t){
         Building building = (Building) t;
         return this.buildingName.compareTo(building.getBuildingName());
